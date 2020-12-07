0f8618ebdefd76e6c89037717675b546a9cb3e90
jonathanleemn <jonathanleemn@gmail.com>
2020-12-06 23:17:34 -0600

    uploaded files

---
+++ templates/top-menu.php
@@ -0,0 +1,17 @@
+<div class="top-menu">
+    <div class="menu-options">
+        <?php if (isLoggedIn()): ?>
+          <!-- adds home link for users -->
+          <a href= "index.php">Home</a>
+          |
+          <a href="list-posts.php">All posts</a>
+           |
+            <a href="edit-post.php">New post</a>
+            |
+            Hello <?php echo htmlEscape(getAuthUser()) ?>.
+            <a href="logout.php">Log out</a>
+        <?php else: ?>
+            <a href="login.php">Log in</a>
+        <?php endif ?>
+    </div>
+</div>
